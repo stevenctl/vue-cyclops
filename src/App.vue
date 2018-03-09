47d1c7a4a9e45a9d8818ae17b553c23bc823a336
slandow <steven.landow@ctl.io>
2018-03-09 11:32:11 -0600

    initial commit - button component

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <cyclops-button @click="onClickThing()" danger>test</cyclops-button>
+  </div>
+</template>
+
+<script>
+import CyclopsButton from './components/CyclopsButton'
+
+export default {
+  name: 'app',
+  components: {
+    'cyclops-button': CyclopsButton
+  },
+  methods: {
+    onClickThing(){
+      alert('hello')
+    }
+  }
+}
+</script>
+
---
+++ src/components/CyclopsButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="$emit('click')" :class="cssClass"><slot></slot></button>
+</template>
+
+<script>
+var types = ['default', 'primary', 'warning', 'danger', 'inverse', 'outline', 'link']
+var sizes = ['lg', 'sm', 'mini']
+
+
+export default {
+  name: 'cyclops-button',
+  props: [...types, ...sizes],
+  computed: {
+      cssClass(){
+          console.log(this.$props)
+          var color = types.find((t) => this.$props[t] != undefined)
+          var size = sizes.find((t) => this.$props[t] != undefined)
+          return "btn" 
+                    + (color ? " btn-" + color : "")
+                    + (size ? " btn-" + size : "")
+      }
+  }
+}
+</script>
+
