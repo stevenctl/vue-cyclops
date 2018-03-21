fdab152cc8c750a9be77094586f30b182220426e
slandow <steven.landow@ctl.io>
2018-03-20 22:52:15 -0500

    enhanced icons (size, color, spin)

--- src/components/CyclopsIcon.vue
+++ src/components/CyclopsIcon.vue
@@ -1,11 +1,23 @@
 <template>
-  <svg class="cyclops-icon" aria-hidden="true"><use v-bind="{'xlink:href' : '#' + icon}"/></svg>
+  <svg :class="cssClass" aria-hidden="true"><use v-bind="{'xlink:href' : '#' + icon}"/></svg>
 </template>
 
 <script>
+var types = ['warning', 'danger', 'green', 'info']
+var sizes = ['md', 'lg', 'xl']
 export default {
   name: 'cyclops-icon',
-  props: ['icon'],
+  props: [...types, ...sizes, 'icon', 'spin'],
+  computed: {
+    cssClass(){
+      var color = types.find((t) => this.$props[t] != undefined)
+      var size = sizes.find((s) => this.$props[s] != undefined)
+      return "cyclops-icon" + 
+                (color ? " " + color : "")
+                + (size ? " " + size : "")
+                + (this.spin != undefined ? " cyclops-spin" : "")
+    }
+  }
 }
 </script>
 
