abe6aa5ba7f02b71cd93385ebbddb10257f64abc
slandow <steven.landow@ctl.io>
2018-04-04 11:34:16 -0500

    fix list view header selection

--- src/components/CyclopsListView.vue
+++ src/components/CyclopsListView.vue
@@ -6,7 +6,7 @@
   <ul class="new-list-view">
     <li v-for="(item, i) in items" :key="i" @click="$emit('itemClicked', item)">
       <div class="list-view-item">
-        <div v-for="(datum, j) in item" :key="j" :class="colCss">{{datum}}</div>
+        <div v-for="(header, j) in headers" :key="j" :class="colCss">{{item[header]}}</div>
       </div>
     </li>
   </ul>
